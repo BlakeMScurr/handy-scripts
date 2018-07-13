29b4f7fe15d833402240898fd110eda6d4afb68a
BlakeMScurr <blakemscurr@gmail.com>
2018-07-13 12:18:00 +1200

    Add convention to update vendor script.

--- update-vendor.sh
+++ update-vendor.sh
@@ -2,6 +2,16 @@
 
 # Currently updates the platform dependency of lingo
 # TODO: make it take args
+parent="lingo"
+vendored="platform"
+if [ -n "$1" ]; then
+    parent=$1
+fi
+if [ -n "$2" ]; then
+    vendored=$2
+fi
 
-rm -rf /home/matt/go/src/github.com/codelingo/lingo/vendor/github.com/codelingo/platform/
-rsync -av /home/matt/go/src/github.com/codelingo/platform /home/matt/go/src/github.com/codelingo/lingo/vendor/github.com/codelingo --exclude .git --exclude bin --exclude vendor --exclude gogs
+echo "replacing vendored" $vendored "of" $parent
+
+rm -rf /home/matt/go/src/github.com/codelingo/$parent/vendor/github.com/codelingo/$vendored
+rsync -av /home/matt/go/src/github.com/codelingo/$vendored /home/matt/go/src/github.com/codelingo/$parent/vendor/github.com/codelingo --exclude .git --exclude bin --exclude vendor --exclude gogs

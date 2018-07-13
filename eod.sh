a7f4eaf9b7ce0a03f340d4219f86327a6dc4fb85
BlakeMScurr <blakemscurr@gmail.com>
2018-07-13 12:17:31 +1200

    EOD script outline.

---
+++ eod.sh
@@ -0,0 +1,8 @@
+# !bin/bash
+
+# TODO: Clean up git repositories
+# Create a script that will tidy up all repos at the end of day,
+# making sure that all master branches are up to date, and I get some insight on 
+# commits that aren't in master.
+# Delete irrelevant branches locally and remotely
+# Check that my remotes suit my convention
